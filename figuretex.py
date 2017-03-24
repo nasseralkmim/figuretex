b0b96b6ad7775c982490d26756490260dc4ed4d0
nasseralkmim <nasser.alkmim@gmail.com>
2017-03-24 08:17:39 -0300

    new params

--- figuretex.py
+++ figuretex.py
@@ -33,22 +33,56 @@ pgf_with_latex = {
     # blank entries should cause plots to inherit fonts from the document
     "font.sans-serif": [],
     "font.monospace": [],
-    "axes.labelsize": 10,
+
     # LaTeX default is 10pt font.
     "font.size": 8,
-    "legend.fontsize": 6,
+    "figure.titlesize":8,
+
+    "legend.fontsize": 7,
+    "legend.markerscale":.9,
+    'legend.numpoints': 1,
+    'legend.handlelength': 2,
+    'legend.scatterpoints': 1,
+    'legend.labelspacing': 0.5,
+    'legend.facecolor':'eff0f1',
+    'legend.edgecolor':'none',
+    'legend.handletextpad': 0.5,  # pad between handle and text
+    'legend.borderaxespad': 0.5,  # pad between legend and axes
+    'legend.borderpad': 0.5,  # pad between legend and legend content
+    'legend.columnspacing': 1,  # pad between each legend column
+
     # Make the legend/label fonts a little smaller
-    "xtick.labelsize": 8,
-    "ytick.labelsize": 8,
-    "figure.figsize": figsize(.9),
+    'xtick.labelsize': 7,
+    'ytick.labelsize': 7,
+    # 'xtick.major.size': 6,
+    # 'xtick.major.size': 6,
+    # 'xtick.minor.size' : 2,
+    # 'xtick.minor.size' : 2,
+
+    'figure.figsize': figsize(.9),
+
     # default fig size of 0.9 textwidth
-    "axes.grid": False,
-    "lines.linewidth": 1,
-    "lines.markersize": 3,
-    "pgf.preamble": [
-        r"\usepackage[utf8x]{inputenc}",
+    "axes.titlesize":8,
+    'axes.grid': True,
+    'axes.spines.left':True,
+    'axes.spines.top':False,
+    'axes.spines.bottom':True,
+    'axes.spines.right':False,
+    'axes.labelsize': 9,
+    'axes.linewidth': 1,
+    # 'axes.xmargin':0,
+    # 'axes.ymargin':0, #no good, it cuts data out
+
+    'grid.linewidth':0.5,
+    'grid.linestyle':':',
+    'lines.linewidth': 1,
+    'lines.markersize': 3,
+
+    'pgf.preamble': [
+        r'\usepackage[utf8]{inputenc}',
+        r'\usepackage{microtype}'
         # use utf8 fonts becasue your computer can handle it :)
-        r"\usepackage[T1]{fontenc}",
+        r'\usepackage[T1]{fontenc}',
         # plots will be generated using this preamble
         ]
     }
@@ -57,12 +91,17 @@ mpl.rcParams.update(pgf_with_latex)
 
 def newfig(width, aspect=None, text_width=390):
     # plt.clf()
-    # plt.style.use('seaborn-paper')
+    plt.style.use('seaborn-muted')
     fig = plt.figure(figsize=figsize(width, aspect, text_width))
     ax = fig.add_subplot(111)
     return fig, ax
 
 
-def savefig(filename):
-    plt.savefig('{}.pgf'.format(filename), bbox_inches='tight')
-    plt.savefig('{}.pdf'.format(filename), bbox_inches='tight')
+def savefig(filename, fig=None):
+    if fig is None:
+        plt.savefig('{}.pgf'.format(filename), bbox_inches='tight')
+        plt.savefig('{}.pdf'.format(filename), bbox_inches='tight')
+    else:
+        fig.savefig('{}.pgf'.format(filename), bbox_inches='tight')
+        fig.savefig('{}.pdf'.format(filename), bbox_inches='tight')
+    
